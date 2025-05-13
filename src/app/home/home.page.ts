6473d9d510335c9f78e79993055e9288db830f28
Mateoilr <milituma@sudamericano.edu.ec>
2025-05-12 22:32:37 -0500

    init

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,40 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
+import { Geolocation } from '@capacitor/geolocation';
+import { Device } from '@capacitor/device';
+import { IonicModule } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonicModule, CommonModule]
 })
 export class HomePage {
+  image: string | undefined;
+  location: any;
+  deviceInfo: any;
+
   constructor() {}
+
+  async takePhoto() {
+    const image = await Camera.getPhoto({
+      quality: 90,
+      allowEditing: false,
+      resultType: CameraResultType.DataUrl,
+      source: CameraSource.Camera
+    });
+
+    this.image = image.dataUrl;
+  }
+
+  async getLocation() {
+    const coordinates = await Geolocation.getCurrentPosition();
+    this.location = coordinates;
+  }
+
+  async getDeviceInfo() {
+    this.deviceInfo = await Device.getInfo();
+  }
 }
